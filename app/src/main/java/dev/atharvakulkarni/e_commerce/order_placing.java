2afee27c8b79d7b4e8911e190792ebe7e7187084
Atharva Kulkarni <atharvakulkarni2204@gmail.com>
2020-10-15 14:05:26 +0530

    order summary section done

--- app/src/main/java/dev/atharvakulkarni/e_commerce/order_placing.java
+++ app/src/main/java/dev/atharvakulkarni/e_commerce/order_placing.java
@@ -2,6 +2,10 @@ package dev.atharvakulkarni.e_commerce;
 
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,6 +16,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class order_placing extends AppCompatActivity
 {
     SeekBar seekBar;
+    Button deliver_here;
+    LinearLayout address,order_summary;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
@@ -19,12 +25,38 @@ public class order_placing extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order_placing);
 
+        getWindow().setStatusBarColor(getResources().getColor(R.color.white,getTheme()));
+
         seekBar = (SeekBar)findViewById(R.id.seekbar);
+        deliver_here = findViewById(R.id.deliver_here);
+        address  = findViewById(R.id.address);
+        order_summary = findViewById(R.id.order_summary);
 
         seekBar.getProgressDrawable().setColorFilter(getResources().getColor(R.color.green2,getTheme()), PorterDuff.Mode.SRC_ATOP);
         seekBar.getThumb().setColorFilter(getResources().getColor(R.color.green2,getTheme()), PorterDuff.Mode.SRC_ATOP);
 
         seekBar.setProgress(10);
+
+        deliver_here.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                address.setVisibility(View.GONE);
+                order_summary.setVisibility(View.VISIBLE);
+
+                seekBar.setProgress(48);
+            }
+        });
+
+        seekBar.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                return true;
+            }
+        });
 
         /*
         order_placing.sliderListener sldListener = new order_placing.sliderListener();
